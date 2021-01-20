ea763a3b15862211aac8e4a376e955dde17c9107
spamz23 <diogo_silva30@hotmail.com>
2021-01-20 20:08:28 +0000

    :rocket: Fix Devops Settings

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -180,6 +180,6 @@ SIMPLE_EMAIL = {
         "EMAIL_SUBJECT": "Welcome to SWOTLab! Please confirm your account",
         "WEBSITE_DOMAIN": "https://swotlab.herokuapp.com",
         "HASH_ID_SALT": os.getenv("SECRET_KEY", "some salt"),
-        "SEND_IN_DEBUG": True
+        "SEND_IN_DEBUG": False
     }
 }
